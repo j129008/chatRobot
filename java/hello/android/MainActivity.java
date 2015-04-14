e9293f3dafb16c89bd261223f30aa92b4f0c5519
j129008 <j129008@gmail.com>
2015-04-14 16:54:35 +0800

    成功讀入中文

--- java/hello/android/MainActivity.java
+++ java/hello/android/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends ActionBarActivity {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
                 intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "請跟機器人說話");
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "zh_TW");
                 try {
                     startActivityForResult(intent, RQS_VOICE_RECOGNITION);
                 }catch (ActivityNotFoundException a) {
